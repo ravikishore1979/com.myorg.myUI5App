6c9f257b372b4c7f67e4d88d52a3aa879a5e3eeb
ravikishore <ravikishore1979@gmail.com>
2019-08-16 19:34:06 +0530

    version change

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,11 @@
 module.exports = function (grunt) {
     'use strict';
     grunt.loadNpmTasks('@sap/grunt-sapui5-bestpractice-build');
+    grunt.config.merge({ compatVersion: "1.38" });
 
     grunt.registerTask('default', [
-        'lint',
         'clean',
+        'lint',
         'build'
     ]);
 };
